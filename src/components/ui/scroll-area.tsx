8bb859b5350a4518a471f43740ad2b164e08cfd6
Sikandar Ali <aliakbar1878048@gmail.com>
2025-05-25 20:03:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: React does not recognize the `thumbClassName` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `thumbclassname` instead. If you accidentally passed it from a parent component, remove it from the DOM element.. Error source: src/components/ui/scroll-area.tsx (12:3) @ _c
    >
    >   10 |   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
    >   11 | >(({ className, children, ...props }, ref) => (
    > > 12 |   <ScrollAreaPrimitive.Root
    >      |   ^
    >   13 |     ref={ref}
    >   14 |     className={cn("relative overflow-hidden", className)}
    >   15 |     {...props}
    >
    > Call Stack
    > 28
    >
    > Show 24 ignore-listed frame(s)
    > div
    > <anonymous> (0:0)
    > _c
    > src/components/ui/scroll-area.tsx (12:3)
    > AppShell
    > src/components/layout/app-shell.tsx (65:7)
    > ApplicationLayout
    > src/app/(app)/layout.tsx (5:10)

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -7,17 +8,17 @@ import { cn } from "@/lib/utils"
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & { thumbClassName?: string }
+>(({ className, children, thumbClassName, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
-    {...props}
+    {...props} // thumbClassName is now destructured and won't be spread here
   >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
+    <ScrollBar thumbClassName={thumbClassName} /> {/* Pass thumbClassName to ScrollBar */}
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
@@ -25,8 +26,8 @@ ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
 
 const ScrollBar = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar> & { thumbClassName?: string }
+>(({ className, orientation = "vertical", thumbClassName, ...props }, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
     orientation={orientation}
@@ -38,9 +39,14 @@ const ScrollBar = React.forwardRef<
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
       className
     )}
-    {...props}
+    {...props} // thumbClassName is destructured and won't be spread here
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb
+      className={cn(
+        "relative flex-1 rounded-full bg-border",
+        thumbClassName // Apply thumbClassName here
+      )}
+    />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
