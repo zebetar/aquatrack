42e0489fce7cbdac50cafbe15d856797abc183d8
Sikandar Ali <aliakbar1878048@gmail.com>
2025-10-31 13:27:08 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: React is not def

--- src/app/(app)/admin/settings/page.tsx
+++ src/app/(app)/admin/settings/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from 'react';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
